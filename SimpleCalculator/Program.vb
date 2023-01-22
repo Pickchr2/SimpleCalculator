05b8a786a810a19e0010665824c77d21724f2a4f
Chris Pickens <Cpickens1270@gmail.com>
2023-01-22 07:54:10 -0700

    Add project files.

---
+++ SimpleCalculator/Program.vb
@@ -0,0 +1,12 @@
+'Christopher Pickens
+'RCET0265
+'Spring 2023
+'Simple Calculator
+'https://github.com/pickchr2/SimpleCalculator.git
+
+Imports System
+
+Module Program
+    Sub Main(args As String())
+    End Sub
+End Module
