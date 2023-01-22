a2b2133a491c4fd0115f7723780e88278f188b2e
Chris Pickens <Cpickens1270@gmail.com>
2023-01-22 08:18:41 -0700

    Accept second user number

--- SimpleCalculator/Program.vb
+++ SimpleCalculator/Program.vb
@@ -12,10 +12,14 @@ Imports System
 Module Program
     Sub Main(args As String())
         Dim firstNumber As Integer
+        Dim secondNumber As Integer
 
         Console.WriteLine("Please enter two whole numbers below that you wish to use for arithmetic...")
         Console.WriteLine("")
         AcceptFirstNumber(firstNumber)
+        Console.WriteLine("Enter second number...")
+        Console.WriteLine("")
+        AcceptSecondNumber(secondNumber)
     End Sub
 
     Function AcceptFirstNumber(_firstNumber As Integer) As Integer
@@ -27,5 +31,16 @@ Module Program
         End Try
 
         Return _firstNumber
+    End Function
+
+    Function AcceptSecondNumber(_secondNumber As Integer) As Integer
+        Try
+            _secondNumber = CInt(Console.ReadLine())
+        Catch ex As Exception
+            Console.WriteLine("Sorry, that is not a whole number.")
+            AcceptFirstNumber(_secondNumber)
+        End Try
+
+        Return _secondNumber
     End Function
 End Module
