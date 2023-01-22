841001e8f43290a0f092d191bf42c09e895e5396
Chris Pickens <Cpickens1270@gmail.com>
2023-01-22 07:59:56 -0700

    Prompt user

--- SimpleCalculator/Program.vb
+++ SimpleCalculator/Program.vb
@@ -4,9 +4,14 @@
 'Simple Calculator
 'https://github.com/pickchr2/SimpleCalculator.git
 
+Option Explicit On
+Option Strict On
+
 Imports System
 
 Module Program
     Sub Main(args As String())
+        Console.WriteLine("Please enter two numbers below that you wish to use for arithmetic...")
+        Console.WriteLine("")
     End Sub
 End Module
